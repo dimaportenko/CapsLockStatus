d7e14c35225ce5f0f5de48b2bcf467744da5e5ff
Dima Portenko <troublesohard@gmail.com>
2023-09-30 23:48:28 +0300

    Initial Commit

---
+++ CapsLockStatus/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CapsLockStatus
+//
+//  Created by Dmitriy Portenko on 30.09.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
